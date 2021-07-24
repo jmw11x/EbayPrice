6dfaae03a0d5e1a3890f50851b341ff31b9b2ba8
jmw11x <jmw11x@gmail.com>
2021-07-24 17:45:43 -0400

    First Commit

---
+++ jwilson177hw1/lib/screens/home/profile.dart
@@ -0,0 +1,215 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:jwilson177hw1/services/adminalert.dart';
+import 'package:jwilson177hw1/services/auth.dart';
+import 'package:link_text/link_text.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DynamicLVP extends StatefulWidget {
+  @override
+  _DynamicLVPState createState() => _DynamicLVPState();
+}
+
+class _DynamicLVPState extends State<DynamicLVP> {
+  AdminAlert db = new AdminAlert();
+  var arr = [];
+  String msg = '';
+  final controller = ScrollController();
+  String cur = '';
+  String url = "https://www.ebay.com/shc";
+  @override
+  Widget build(BuildContext context) {
+    Future<String> current_user = db.currentuid();
+    current_user.then((value) => setState(() {
+          cur = value;
+        }));
+
+    Future<String> msgs = db.getWatchItemAsString(cur);
+    msgs.then((value) => setState(() {
+          msg = value;
+          // print(msg);
+        }));
+    arr = msg.split('\n');
+    // autoadj();
+    return ListView.builder(
+        reverse: true,
+        controller: controller,
+        itemCount: arr.length,
+        itemBuilder: (context, index) {
+          final reversedIndex = arr.length - 1 - index;
+          String item = arr[reversedIndex];
+          print(item.split(" ")[0]);
+
+          return Container(
+            child: InkWell(
+              child: new Text(
+                arr[reversedIndex] + "\n\n",
+                style: TextStyle(
+                  color: Colors.lime,
+                  fontSize: 29,
+                ),
+              ),
+              onTap: () {
+                launch("https://www.ebay.com/sch/" + item.split(":")[0]);
+              },
+            ),
+          );
+        });
+  }
+}
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  String uid = '';
+  String firstName = '';
+  String social = "";
+  String lastName = '';
+  @override
+  Widget build(BuildContext context) {
+    final AuthService _auth = AuthService();
+    AdminAlert db = new AdminAlert();
+
+    Future<String> uzr = db.currentuid();
+    uzr.then((value) => setState(() {
+          uid = value;
+        }));
+    Future<String> soc = db.getsocial();
+    soc.then((value) => setState(() {
+          social = value;
+        }));
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[400],
+        elevation: 0.0,
+        title: Text('Profile Settings'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (v) => v!.isEmpty ? 'cannot leave empty' : null,
+                decoration: InputDecoration(
+                    hintText: 'firstname', helperText: 'Change first name'),
+                onChanged: (v) => {setState(() => firstName = v)},
+              ),
+              TextFormField(
+                validator: (v) => v!.isEmpty ? 'cannot leave empty' : null,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'last name', helperText: 'reset last name'),
+                onChanged: (v) => {setState(() => lastName = v)},
+              ),
+              ElevatedButton(
+                  child: Text('Reset Name'),
+                  onPressed: () async {
+                    await db.updateProfile(firstName, uid, lastName);
+                  }),
+              ElevatedButton(
+                  child: Text('Load watch list'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => WatchList()),
+                    );
+                  }),
+              LinkText(
+                social,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WatchList extends StatefulWidget {
+  const WatchList({Key? key}) : super(key: key);
+
+  @override
+  _WatchListState createState() => _WatchListState();
+}
+
+class _WatchListState extends State<WatchList> {
+  String message = '';
+  String msg = '';
+  String current_user = '';
+  String name = '';
+  String docid = '';
+
+  bool cexists = false;
+  bool cexists1 = false;
+  var arr = [];
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth1 = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    final AuthService _auth = AuthService();
+    AdminAlert db = new AdminAlert();
+
+    Future<String> uzr = db.currentuid();
+    Future<String> username = db.getUser();
+
+    // msgs.then((value) => setState(() {
+    //       msg = value;
+    //     }));
+    uzr.then((value) => setState(() {
+          current_user = value;
+        }));
+    username.then((value) => setState(() {
+          name = value;
+        }));
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("WATCHLIST"),
+          backgroundColor: Colors.greenAccent,
+          elevation: 0.0,
+          actions: <Widget>[
+            TextButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Expanded(child: DynamicLVP()),
+              Container(
+                child: TextFormField(
+                  validator: (v) => v!.isEmpty ? 'cannot leave empty' : null,
+                  decoration: InputDecoration(
+                      hintText: 'message', helperText: 'search'),
+                  onChanged: (v) => {setState(() => message = v)},
+                ),
+              ),
+              SingleChildScrollView(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      //search here
+                      print("Search for" + message);
+                      db.search(message);
+                      FocusScope.of(context).unfocus();
+                    },
+                    child: Text(">Search")),
+              )
+            ],
+          ),
+        ));
+  }
+}
---
+++ jwilson177hw1/lib/services/adminalert.dart
@@ -0,0 +1,180 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:jwilson177hw1/models/user.dart';
+
+class AdminAlert {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final CollectionReference messages =
+      FirebaseFirestore.instance.collection('messages');
+  final Query messages1 =
+      FirebaseFirestore.instance.collection('messages').orderBy('datetime');
+  final CollectionReference users =
+      FirebaseFirestore.instance.collection('users');
+  final CollectionReference items =
+      FirebaseFirestore.instance.collection('items');
+  Future<String> getUser() async {
+    User uid = await _auth.currentUser!;
+    String user_id = uid.uid.toString();
+    DocumentSnapshot ds = await users.doc(user_id).get();
+    String name = ds.get('firstName');
+    return name;
+  }
+
+  Future<String> getsocial() async {
+    User uid = await _auth.currentUser!;
+    String user_id = uid.uid.toString();
+    DocumentSnapshot ds = await users.doc(user_id).get();
+    String name = ds.get('social');
+    return name;
+  }
+
+  Future<String> currentuid() async {
+    User uid = await _auth.currentUser!;
+    String user_id = uid.uid.toString();
+    return user_id;
+  }
+
+  Future<String> getUsersAsString() async {
+    String registered = '';
+    await users.get().then((message) {
+      message.docs.forEach((value) {
+        if (value['firstName'] != null) {
+          registered = registered + value.id + ", " + value['firstName'] + "\n";
+        }
+      });
+    });
+    return registered;
+  }
+
+  Future<String> getBoardsAsString() async {
+    String avaialble = '';
+    await items.get().then((message) {
+      message.docs.forEach((value) {
+        if (value['items'] != null) {
+          avaialble = avaialble + value['items'] + "\n";
+        }
+      });
+    });
+    return avaialble;
+  }
+
+  Future<bool> chatExists(String other, String current) async {
+    final snapshot =
+        await FirebaseFirestore.instance.collection(other + current).get();
+    final snapshot1 =
+        await FirebaseFirestore.instance.collection(current + other).get();
+    if (snapshot.docs.length == 0) {
+      return false;
+    }
+    return true;
+  }
+
+  Future<bool> chatExists1(String other, String current) async {
+    final snapshot =
+        await FirebaseFirestore.instance.collection(other + current).get();
+    final snapshot1 =
+        await FirebaseFirestore.instance.collection(current + other).get();
+    if (snapshot.docs.length == 0 || snapshot1.docs.length == 0) {
+      return false;
+    }
+    return true;
+  }
+
+  Future<String> search(String current) async {
+    String msgs = '';
+    // print(current);
+    final Query chats1 = FirebaseFirestore.instance
+        .collection(current)
+        .where('item', isEqualTo: current);
+    // print(current);
+    await chats1.get().then((message) {
+      message.docs.forEach((value) {
+        // print("54" + value["message"]);
+        if (value["item"] != null)
+          msgs = msgs +
+              value["item"] +
+              " " +
+              value["condition"] +
+              ": " +
+              (value['price']) +
+              "\n";
+        // print(msgs);
+      });
+    });
+    return msgs;
+  }
+
+  Future<String> getMessagesAsString(String current) async {
+    print(current + "MSG");
+    String msgs = '';
+    // print(current);
+    final CollectionReference qry =
+        FirebaseFirestore.instance.collection(current);
+    // print(current);
+    await qry.get().then((message) {
+      message.docs.forEach((value) {
+        // print("54" + value["message"]);
+        if (value["item"] != null)
+          msgs = msgs +
+              value["item"] +
+              " " +
+              value["condition"] +
+              ": " +
+              value['price'] +
+              "\n";
+        // print(msgs);
+      });
+    });
+    return msgs;
+  }
+
+  Future<String> getWatchItemAsString(String current) async {
+    print(current + "MSG");
+    String msgs = '';
+    // print(current);
+    final CollectionReference qry =
+        FirebaseFirestore.instance.collection(current);
+    // print(current);
+    await qry.get().then((message) {
+      message.docs.forEach((value) {
+        // print("54" + value["message"]);
+        if (value["item"] != null) msgs = msgs + value["item"] + "\n";
+        // print(msgs);
+      });
+    });
+    return msgs;
+  }
+
+  Future addAdminMessage(String message, String current, String name) async {
+    await FirebaseFirestore.instance.collection(current).doc().set({
+      'Message': message,
+      'datetime': DateTime.now().toLocal().toString(),
+      'author': name
+    });
+  }
+
+  Future addItem(String message, String current) async {
+    await FirebaseFirestore.instance.collection(current).doc().set({
+      'item': message,
+    });
+  }
+
+  Future updateProfile(String first, String current, String last) async {
+    await FirebaseFirestore.instance.collection(current).doc().update({
+      'firstName': first,
+      'lastname': last,
+    });
+  }
+
+  Future updatesocial(String social, String current) async {
+    await FirebaseFirestore.instance.collection('users').doc(current).update({
+      'social': social,
+    });
+  }
+
+  Future<void> updateEmail(String email) async {
+    return _auth.sendPasswordResetEmail(email: email);
+  }
+}
